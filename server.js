8e9a4b021f52dc6d304891e4876d5d5d45d8c763
Pooja R <poojarmacbook@Poojas-Air.lan>
2025-03-16 15:24:53 +0530

    Details API added

--- server.js
+++ server.js
@@ -20,4 +20,22 @@ app.get("/api/swiggy", async (req, res) => {
     }
 });
 
+
+app.get("/api/swiggy/details/:id", async (req, res) => {
+    try {
+        const response = await fetch(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9124323&lng=77.57087229999999&restaurantId=${req.params.id}`,{
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.0.0 Safari/537.36",
+                "Accept": "application/json",
+            }
+        });
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
+
+    
+})
+
 app.listen(5000, () => console.log("Server running on port 5000"));
