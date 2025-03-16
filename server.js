05e3cdb272912ba5ff1428f91555bf367ed65cdc
Pooja R <poojarmacbook@Poojas-Air.lan>
2025-03-16 14:33:06 +0530

    url updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 
 app.get("/api/swiggy", async (req, res) => {
     try {
-        const response = await fetch("https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9124323&lng=77.57087229999999&str=restaurants");
+        const response = await fetch("https://www.swiggy.com/dapi/restaurants/search/v3?lat=12.9124323&lng=77.57087229999999&str=restaurants&trackingId=7d9b519d-0598-d583-bce4-b5f6a39fcc5f&submitAction=ENTER&queryUniqueId=e910bd49-c203-0ed3-a121-6f0cbab8dfb6");
         const data = await response.json();
         res.json(data);
     } catch (error) {
